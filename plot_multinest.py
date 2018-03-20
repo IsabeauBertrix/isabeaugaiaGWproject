3ee940b19a0edfa75a2c99d936133d600b73473b
IsabeauBertrix <isabeau.bertrix@sfr.fr>
2018-03-20 10:54:09 +0000

    Hello again

--- plot_multinest.py
+++ plot_multinest.py
@@ -13,10 +13,11 @@ from scipy import stats
 from pymultinest.solve import Solver
 from scipy.special import ndtri 
 
+
 def oneDhist( chain, minetmax ):
     kernel = stats.gaussian_kde( chain )
     hist, bin_edges = np.histogram(chain)
-    values = np.arange(minetmax[0],minetmax[1],(minetmax[1]-minetmax[0])/1000.)
+    values = np.linspace(minetmax[0],minetmax[1],1000)
     area = len(chain) * ( bin_edges[1] - bin_edges[0] )
     plt.hist(chain, bins = bin_edges)
     plt.plot( values , kernel(values) * area )
@@ -24,8 +25,27 @@ def oneDhist( chain, minetmax ):
     plt.show()
     return 1
     
-def twoDhist( chain1 , chain2 ):
-    plt.hist(chain1, chain2)
+def twoDhist( chain1 , chain2 , minetmax1, minetmax2):
+
+    gridx = np.linspace(minetmax1[0],minetmax1[1],50)
+    gridy = np.linspace(minetmax2[0],minetmax2[1],50)
+
+    grid, _, _ = np.histogram2d(chain1, chain2, bins=[gridx, gridy])
+    left, width = 0.12, 0.55
+    bottom, height = 0.12, 0.55
+    bottom_h = left_h = left+width+0.02
+ 
+    # Set up the geometry of the three plots
+    rect_temperature = [left, bottom, width, height] # dimensions of temp plot
+    rect_histx = [left, bottom_h, width, 0.25] # dimensions of x-histogram
+    rect_histy = [left_h, bottom, 0.25, height] # dimensions of y-histogram
+ 
+    # Set up the size of the figure
+    fig = plt.figure(1, figsize=(9.5,9))
+
+    axTemperature = plt.axes(rect_temperature)
+    axTemperature = plt.pcolormesh(gridx, gridy, grid, cmap = 'Greys')
+    #axTemperature = plt.axis([minetmax1[0],minetmax1[1], minetmax2[0], minetmax2[1]])
     plt.show()
     return 1
 
@@ -39,21 +59,12 @@ filename2 = "out/minetmax.dat"
 minetmax = np.loadtxt(filename2)
 
 # loop over params to produce 1D histograms
-for i in range(npar - 1): 
-    oneDhist(chains[i], minetmax[i])
+#for i in range(npar - 1): 
+ #   oneDhist(chains[i], minetmax[i])
 
 # double loop over params to produce 2D histograms
-for i in range(npar):
-    for j in range(npar):
-        twoDhist(chains[i], chains[j])
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+for i in range(2):
+    for j in range(2):
+        if j != i:
+            twoDhist(chains[i], chains[j], minetmax[i], minetmax[j])
         
