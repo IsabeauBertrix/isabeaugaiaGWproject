a62f82de22678b5c56ec79e662c2606fac1078dc
IsabeauBertrix <isabeau.bertrix@sfr.fr>
2018-04-24 14:56:23 +0100

    death

--- ONECODETORULETHEMALL.py
+++ ONECODETORULETHEMALL.py
@@ -12,16 +12,33 @@ import matplotlib.pyplot as plt
 import re
 import os
 from sys import version
-
+from scipy import stats
 from collections import namedtuple
  
 c = 2.99e8
 w = np.pi/2
    
-def gen_rand_point(): 
-    pi = np.random.normal(0, 1, 3)
-    modulus_sqr_pi = np.dot(pi,pi)
-    return pi/np.sqrt(modulus_sqr_pi)
+GW_parameters = namedtuple("GW_parameters", "logGWfrequency logAmplus logAmcross cosTheta Phi DeltaPhiPlus DeltaPhiCross")
+
+def gen_rand_GW():
+    
+    period = np.random.uniform( 3600 * 24 * 28, 3600 * 24 * 365.25 * 5)
+
+    log10Amplus = np.random.uniform( -15, -10 )
+    log10Amcross = np.random.uniform( -15, -10 )
+    A = np.sqrt( np.power(np.power(10, log10Amcross) , 2) + np.power(np.power(10, log10Amplus) , 2))
+    log10Amplus = log10Amplus - 12 - np.log10(A)
+    log10Amcross = log10Amcross - 12 - np.log10(A)
+
+    cosTheta = np.random.uniform(-1 , 1 )
+    Phi = np.random.uniform(0 , 2 * np.pi )
+    
+    DeltaPhiPlus = np.random.uniform( 0 , 2 * np.pi )
+    DeltaPhiCross = np.random.uniform( 0 , 2 * np.pi )   
+
+    GW_par = GW_parameters( logGWfrequency = np.log( 2 * np.pi / period ) , logAmplus = log10Amplus, logAmcross = log10Amcross, cosTheta = cosTheta, Phi = Phi, DeltaPhiPlus = DeltaPhiPlus , DeltaPhiCross = DeltaPhiCross )
+    return GW_par   
+  
     
 def LoadData( filename ):
 
@@ -34,7 +51,7 @@ def LoadData( filename ):
 
     data = []
 
-    for i in range( 10 ) : #len( content ) ):
+    for i in range( len( content ) ):
 
         line = content[i]
         line = re.split(', \[|\], \]|\]',line)
@@ -179,7 +196,7 @@ def calculate_timing_residuals_complicated ( star_positions_times_angles , GW_pa
     
     return np.array( x )
       
-GW_parameters = namedtuple("GW_parameters", "logGWfrequency logAmplus logAmcross cosTheta Phi DeltaPhiPlus DeltaPhiCross")
+
 GW_par = GW_parameters( logGWfrequency = np.log( 2 ) - 7 * np.log(10), logAmplus = np.log(3) - 14*np.log(10), logAmcross = np.log(3) - 14*np.log(10), cosTheta = 0.5, Phi = 1.0, DeltaPhiPlus = 1*np.pi , DeltaPhiCross = np.pi/2. )
     
 
@@ -601,21 +618,76 @@ def fisher_matrix4 (star_positions_times_angles , GW_par, sigma_t, distances):
         for j in range( len(star_positions_times_angles[i][1])): #len of the times
             M = matrix_derivative4( star_positions_times_angles[i][0], star_positions_times_angles[i][1][j] * 1.0e-9 , star_positions_times_angles[i][2][j], GW_par, distances[i])
             Sigma4 = Sigma4 + M / (sigma_t * sigma_t )
-    return(Sigma4) 
-         
-GW_par = GW_parameters( logGWfrequency = np.log(2*np.pi/(3*28*24*3600.)), logAmplus = -12*np.log(10), logAmcross = -12*np.log(10), cosTheta = 0.5, Phi = 1.0, DeltaPhiPlus = 1 * np.pi , DeltaPhiCross = 1 * np.pi )          
-#test_derivatives1(GW_par )
-#test_derivatives2(GW_par, 1.0e16 )
+    return(Sigma4)
+    
 star_positions_times_angles = LoadData( "MockAstrometricTimingData/gwastrometry-gaiasimu-1000-randomSphere-v2.dat" )
 sigma = 2.9e-13 
 sigma_t = 1.0e-9 
 distances = np.random.normal(3.086e16 , 1.0e13, len(star_positions_times_angles))
 
-from numpy import linalg as LA
 
-Sigma1 = fisher_matrix1 (star_positions_times_angles , GW_par, sigma)    
-w1,v1 = LA.eigh( Sigma1 )
-invSigma1 = np.dot( v1 , np.dot( np.diag(1./w1) , np.transpose(v1) )  )
+
+def WapperFunction_FisherMatrix ( args ):
+    
+    star_positions_times_angles = args[0]
+    sigma = args[1]
+
+    GW_par = gen_rand_GW ()
+    
+    SIGMA = fisher_matrix1 (star_positions_times_angles , gen_rand_GW() , sigma )
+    
+    return [ GW_par , SIGMA ]
+    
+def Save_Results_To_File ( results , filename ):
+    with open(filename, 'w') as f:
+        f.write(str(results)) 
+    return 1
+
+from multiprocessing import Pool
+num = 100
+n_cpus = 48
+argument_list = [ [star_positions_times_angles , sigma ] for i in range(num) ]
+p = Pool ( n_cpus )
+results = p.map ( WapperFunction_FisherMatrix , argument_list )
+Save_Results_To_File ( results , "test.dat" )
+exit(-1)
+    
+
+def oneDhist_automatic_xrange( chain ):
+    #kernel = stats.gaussian_kde( chain )
+    hist, bin_edges = np.histogram(chain)
+    #area = len(chain) * ( bin_edges[1] - bin_edges[0] )
+    plt.hist(chain, bins = bin_edges)
+    plt.show()
+    return 1
+
+#mm = np.zeros((7,10))
+#mm = [[min(jean_nez_marre[:,i]), max(jean_nez_marre[:,i])] for i in range(7)]
+
+oneDhist_automatic_xrange(matrix[:,0] )
+plt.savefig("/home/isabeau/Documents/Cours/isabeaugaiaGWproject/fisher_matrix/logfrenquency.png")
+oneDhist_automatic_xrange(matrix[:,1] )
+plt.savefig("/home/isabeau/Documents/Cours/isabeaugaiaGWproject/fisher_matrix/logAmplus.png")
+oneDhist_automatic_xrange(matrix[:,2] )
+plt.savefig("/home/isabeau/Documents/Cours/isabeaugaiaGWproject/fisher_matrix/logAmcross.png")
+oneDhist_automatic_xrange(matrix[:,3] )
+plt.savefig("/home/isabeau/Documents/Cours/isabeaugaiaGWproject/fisher_matrix/costheta.png")
+oneDhist_automatic_xrange(matrix[:,4] )
+plt.savefig("/home/isabeau/Documents/Cours/isabeaugaiaGWproject/fisher_matrix/phi.png")
+oneDhist_automatic_xrange(matrix[:,5] )
+plt.savefig("/home/isabeau/Documents/Cours/isabeaugaiaGWproject/fisher_matrix/deltaphiplus.png")
+oneDhist_automatic_xrange(matrix[:,6] )
+plt.savefig("/home/isabeau/Documents/Cours/isabeaugaiaGWproject/fisher_matrix/deltaphicross.png*")
+
+
+"""         
+#GW_par = GW_parameters( logGWfrequency = np.log(2*np.pi/(3*28*24*3600.)), logAmplus = -12*np.log(10), logAmcross = -12*np.log(10), cosTheta = 0.5, Phi = 1.0, DeltaPhiPlus = 1 * np.pi , DeltaPhiCross = 1 * np.pi )          
+#test_derivatives1(GW_par )
+#test_derivatives2(GW_par, 1.0e16 )
+
+
+
+
 
 Sigma2 = fisher_matrix2 (star_positions_times_angles , GW_par, sigma, distances)    
 w2,v2 = LA.eigh( Sigma2 )
@@ -648,6 +720,7 @@ if reponse in ['1', '2']:
                 print('well ok... ʕ•͡-•ʔ ')
             print('( ͡° ͜ʖ ͡°)')
             error = np.sqrt(np.diag(invSigma1))
+            print(error)
         if reponse2 == 'b':
             reponse3 = raw_input("Oh wait, do you also want to see the plots? [yes/no]:")
             if reponse3 == 'yes':
@@ -656,15 +729,17 @@ if reponse in ['1', '2']:
                 print('well ok... ʕ•͡-•ʔ ')
             print('(ง ͡ʘ ͜ʖ ͡ʘ)ง')
             error = np.sqrt(np.diag(invSigma2))
+            print(error)
     if reponse == '2':
         if reponse2 == 'a':
             reponse3 = raw_input("Oh wait, do you also want to see the plots? [yes/no]:")
             if reponse3 == 'yes':
-                test_derivatives3(GW_par)
+                test_derivatives3(gen_rand_GW())
             if reponse3 == 'no':
                 print('well ok... ʕ•͡-•ʔ ')
             print('Well...You know, people assume that time is a strict progression of cause to effect, but actually from a non-linear, non-subjective viewpoint, it s more like a big ball of wibbly wobbly... time-y wimey... stuff')
             error = np.sqrt(np.diag(invSigma3))
+            print(error)
         if reponse2 == 'b':
             reponse3 = raw_input("Oh wait, do you also want to see the plots? [yes/no]:")
             if reponse3 == 'yes':
@@ -673,13 +748,24 @@ if reponse in ['1', '2']:
                 print('well ok... ʕ•͡-•ʔ ')
             print('Congrats, all the Universe, will explose')
             error = np.sqrt(np.diag(invSigma4))
+            print(error)
 elif reponse == '42':
     print('je suis un ordinateur d une si infiniment subtile complexite que la vie organique elle même fait partie de mes unités de calculs. Et vous mêmes prendrez de nouvelles formes, plus primitives, et pénétreraient dans l ordinateur pour naviguer le long des dix millions d années de son programme. Je construirai cet ordinateur pour vous, et le nommerai' )
     print('DO NOT FORGET YOUR TOWEL, NEVER')
     print('Alien are afraid of towel')
     print('For instance, on the planet Earth, man had always assumed that he was more intelligent than dolphins because he had achieved so much—the wheel, New York, wars and so on—whilst all the dolphins had ever done was muck about in the water having a good time. But conversely, the dolphins had always believed that they were far more intelligent than man—for precisely the same reasons.')
+elif reponse == '66':
+    reponse4 = raw_input("Did you ever hear the trajedy of Dark Plagueis The Wise? [yes/no]:   ")
+    if reponse4 == 'yes':
+        print("OMG YOU ARE THE DARK LORD OF THE SITH!!!!!!! (；☉_☉)")
+        reponse5 = raw_input("Execute order 66? [yes/no]:    ")
+        if reponse5 == 'yes':
+            print("(✖╭╮✖)")
+        if reponse5 == 'no':
+            print("┌(˘⌣˘)ʃ")
+    if reponse4 == 'no':
+        print("I thought not. It's not a story the jedi will tell you. It's a Sith legend. Dark Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life ... He had such a knowledge of the dark side, he could even keep the ones he cared about from dying." )
 else:
     print ("Good aswer.")
     print("Thank you for your participation, we hope you will have a great life. Please enjoy your day")
-print(error)
-exit(-1)      
+"""
